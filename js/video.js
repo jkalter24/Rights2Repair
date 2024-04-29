3429dba85dd7938487e1866b08cc93b4003f8cf3
Joseph <70232140+jkalter24@users.noreply.github.com>
2024-04-29 00:54:59 -0400

    del

--- js/video.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.video-thumbnails img').forEach(img => {
-        img.addEventListener('click', function() {
-            document.getElementById('main-video').src = this.dataset.videoSrc;
-        });
-    });
-  });
-  
